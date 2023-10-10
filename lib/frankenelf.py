7079a8606adec5efce12951dd79b6e9ff1ad5f4a
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-10-09 20:45:11 -0500

    Update frankenelf.py

--- lib/frankenelf.py
+++ lib/frankenelf.py
@@ -1,14 +1,19 @@
 import sys
 
-with open(sys.argv[1], 'r+b') as file:
-    assert file.read(5) == b'\xeb\x0bELF'
-    file.seek(0)
-    file.write(b'\xeb\x0bPLD')
-    file.seek(64+56+16)
-    vaddr = int.from_bytes(file.read(8), 'little')
-    file.seek(64+56+32)
-    filesz = (int.from_bytes(file.read(8), 'little') - vaddr).to_bytes(8, 'little')
-    memsz = (int.from_bytes(file.read(8), 'little') - vaddr).to_bytes(8, 'little')
-    file.seek(64+56+32)
-    file.write(filesz)
-    file.write(memsz)
+def main():
+    with open(sys.argv[1], 'r+b') as file:
+        print(f'{__file__} start')
+        assert file.read(5) == b'\xeb\x0bELF'
+        file.seek(0)
+        file.write(b'\xeb\x0bPLD')
+        file.seek(64+56+16)
+        vaddr = int.from_bytes(file.read(8), 'little')
+        file.seek(64+56+32)
+        filesz = (int.from_bytes(file.read(8), 'little') - vaddr).to_bytes(8, 'little')
+        memsz = (int.from_bytes(file.read(8), 'little') - vaddr).to_bytes(8, 'little')
+        file.seek(64+56+32)
+        file.write(filesz)
+        file.write(memsz)
+
+if __name__ == '__main__':
+    main()
